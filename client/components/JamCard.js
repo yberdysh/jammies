c18950e0be118b1490c9f5ad01131fb98e7bfcc9
Jane Costa <yberdysh@gmail.com>
2018-11-17 17:15:37 -0500

    fixed flexbox card styling and added semantic ui

--- client/components/JamCard.js
+++ client/components/JamCard.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-import { Card, Image } from 'semantic-ui-react';
+// import { Card, Image } from 'semantic-ui-react';
 import {Link} from 'react-router-dom'
+import { Card, Icon, Image } from 'semantic-ui-react'
 // import {Grid, Col, Row} from 'react-bootstrap'
 
 export class JamCard extends Component {
 
   render() {
+    console.log("what is jam", jam)
     const { jam } = this.props
     return (
       <Link to={`/jams/${jam.id}`}>
-        <div className="card" style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url(${this.props.jam.image})`}}>
-          <div className="card-category">Popular</div>
-          <div className="card-description">
-            <h2>Name</h2>
-            <p>Very cool city, the best</p>
-          </div>
-      </div>
+        <Card>
+          <Image src={jam.image} />
+          <Card.Content>
+            <Card.Header>Name</Card.Header>
+            <Card.Description>Very cool city, the best</Card.Description>
+          </Card.Content>
+        </Card>
       </Link>
     )
   }
-
 }
+
+
+
