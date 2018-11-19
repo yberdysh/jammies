3db0f18e9a9b0ddbf495c1c8c3529b10e8533baf
Jane Costa <yberdysh@gmail.com>
2018-11-18 20:12:28 -0500

    added price to card

--- client/components/JamCard.js
+++ client/components/JamCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'
 // import { Card, Image } from 'semantic-ui-react';
 import {Link} from 'react-router-dom'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 // import {Grid, Col, Row} from 'react-bootstrap'
 
 export class JamCard extends Component {
@@ -10,15 +10,15 @@ export class JamCard extends Component {
   render() {
     const { jam } = this.props
     return (
-      <Link to={`/jams/${jam.id}`}>
-        <Card>
+      <Card>
+        <Link to={`/jams/${jam.id}`}>
           <Image src={jam.image} />
+        </Link>
           <Card.Content>
-            <Card.Header>Name</Card.Header>
-            <Card.Description>Very cool city, the best</Card.Description>
+            <Card.Header>{jam.name}</Card.Header>
+            <Card.Meta>{`$${jam.price}`}</Card.Meta>
           </Card.Content>
         </Card>
-      </Link>
     )
   }
 }
