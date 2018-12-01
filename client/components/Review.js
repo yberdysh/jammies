7ee7f9d3610536e181a31b5bbbfece57b67cbada
Jane Costa <yberdysh@gmail.com>
2018-12-01 18:16:20 -0500

    used font awesome to create stars

--- client/components/Review.js
+++ client/components/Review.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export class Review extends Component {
-  render() {
-    return (
-      <div><p>This is a review</p></div>
-    );
+
+function Review({review}) {
+  const countStars = function(numStars){
+    // this countStars function will only include whole numbers since it's
+    // num stars from review, and users can only give whole numbers in review
   }
+  console.log("count stars:", countStars(review.numStars))
+
+    return (
+      <div>{review.content}
+        <i className="fas fa-star"/>
+      </div>
+    );
 }
+
+export default Review
--- client/components/SelectedJam.js
+++ client/components/SelectedJam.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {fetchSingleJam} from '../store'
+import Review from './Review'
 
 export class SelectedJam extends Component {
     componentDidMount(){
@@ -8,15 +9,24 @@ export class SelectedJam extends Component {
       this.props.fetchSingleJam(jamId)
     }
 
+  countStars = (numStars) => {
+    // function for determining num stars for a jam based on reviews
+    // this will include decimals
+    console.log(numStars)
+    const wholeNum = numStars.toString().slice(0, 1)
+    const decimal = numStars - parseInt(wholeNum)
+  }
+
     render(){
       const {selectedJam} = this.props
+      // console.log("selecetedJam")
       console.log("selectedJam", selectedJam)
       return (
         <div className="selectedJam">
           <h1>{selectedJam.name}</h1>
           <img src={selectedJam.image} alt=""/>
           <p>{selectedJam.description}</p>
-          <h2>Reviews: {selectedJam.reviews.map(review => <Review />)}</h2>
+          <h2>Reviews: {selectedJam.reviews && selectedJam.reviews.map(review => <Review key={review.id} review={review}/>)}</h2>
         </div>
       );
     }
