9f1f4a53d9324f49ec40ca7cb47f3a7824351dde
Jane Costa <yberdysh@gmail.com>
2018-12-01 18:41:12 -0500

    fixed classnames and key issue

--- client/components/Review.js
+++ client/components/Review.js
@@ -6,12 +6,13 @@ function Review({review}) {
   const countStars = function(numStars){
     const solidStars = numStars
     const emptyStars = 5 - numStars
+    let key = 0;
     let iconsToRender = [];
     _.times(solidStars, () => {
-      iconsToRender.push(<i className="fas fa-star"/>)
+      iconsToRender.push(<i key={++key} className="fas fa-star"/>)
     });
     _.times(emptyStars, () => {
-      iconsToRender.push(<i class="far fa-star"/>)
+      iconsToRender.push(<i key={++key} className="far fa-star"/>)
     });
     console.log('icons to render', iconsToRender)
     return iconsToRender
