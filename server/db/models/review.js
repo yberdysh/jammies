da67081003b197c04cffa3e56540c8507c3cde39
Jane Costa <yberdysh@gmail.com>
2018-12-02 12:28:56 -0500

    cleaned up review on model

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -17,25 +17,14 @@ const Review = db.define('review', {
 })
 
 Review.hook('afterCreate', async review => {
-  console.log("DAT JAM", Jam)
-  console.log('REVIEW', review)
   try {
     const jam = await Jam.findById(review.jamId)
     let reviewsRes = await Review.findAll({
       where: {jamId: review.jamId}
     })
-    console.log("REVIEWS RES", reviewsRes)
-
-    console.log("REVIERS RES ARRAY?", Array.isArray(reviewsRes))
-
-    console.log("OBJECT KEYS OF REVIEWS RES", Object.keys(reviewsRes))
-
     const reviews = Object.keys(reviewsRes).map(
         review => reviewsRes[review].dataValues
       )
-
-    console.log("REVIEWS", reviews)
-
     const count = reviews.length
     const sum = reviews.reduce((accum, currentVal) => {
         return accum + currentVal.numStars
@@ -45,45 +34,6 @@ Review.hook('afterCreate', async review => {
   } catch(e) {
     console.error(e)
   }
-
-
-  // if (reviews.length){
-  //   const count = reviews.length
-  //   const sum = reviews.reduce((accum, currentVal) => {
-  //     return accum + currentVal.numStars
-  //   }, 0)
-  //   const average = _.round(sum/count, 1)
-  //   return average
-  // } else {
-  //   return 0;
-  // }
-});
-
-// const averageRating = async review => {
-//   try {
-//     // pass in the review right after its created
-//     const jellyP = Jelly.findById(review.jellyId)
-//     // find the jelly by the review's jelly id
-//     const reviewsP = Review.findAll({
-//       where: {jellyId: review.jellyId}
-//     })
-//     // find all the reviews for that jellyID
-
-//     const [jellyRes, reviewsRes] = await Promise.all([jellyP, reviewsP])
-
-//     const reviews = Object.keys(reviewsRes).map(
-//       keys => reviewsRes[keys].dataValues
-//     )
-
-//     let rating = reviews.reduce((a, b) => a + b.starRating, 0) / reviews.length
-//     rating = Math.round(rating * 10) / 10
-
-//     jellyRes.update({rating})
-//   } catch (e) {
-//     console.error(e)
-//   }
-// }
-
-// Review.afterCreate(averageRating)
+})
 
 module.exports = Review
