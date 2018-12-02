beef33d60fd5607ba494b2bff6db454a2d06f628
Jane Costa <yberdysh@gmail.com>
2018-12-02 12:30:47 -0500

    cleaned up reviews on api routes

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -5,9 +5,6 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     let reviews = await Review.findAll({})
-    // console.log("REVIEWS INSIDE ROUTES", reviews.length)
-    console.log("length of reviews", reviews.length)
-    console.log("data type of reviews", Array.isArray(reviews))
     res.json(reviews)
   } catch(err) {
     next(err)
