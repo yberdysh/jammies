14d14c0191255b7e9d1245e26e230a4fe05c4f6a
Jane Costa <yberdysh@gmail.com>
2018-12-09 21:46:02 -0500

    deciding on style for text under card

--- client/components/JamCard.js
+++ client/components/JamCard.js
@@ -16,9 +16,11 @@ export class JamCard extends Component {
           <Image src={jam.image} />
         </Link>
           <Card.Content>
-            <Card.Header>{jam.name}</Card.Header>
-            <Card.Meta >{starsArray}({jam.reviews.length} reviews {formatNumStars(jam.numStars)})</Card.Meta>
-            <Card.Meta>{`$${jam.price}`}</Card.Meta>
+            <div className="jamCard">
+              <Card.Header className="jamName">{jam.name}</Card.Header>
+              <Card.Meta >{starsArray}({jam.reviews.length} reviews {formatNumStars(jam.numStars)})</Card.Meta>
+              <Card.Meta>{`$${jam.price}`}</Card.Meta>
+              </div>
           </Card.Content>
         </Card>
     )
